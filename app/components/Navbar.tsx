c898c3e90c90696cf9aae6fcdeab02b0d632d557
greenlime21 <greenlime21@gmail.com>
2020-12-29 16:42:19 +0900

    pagesの不要なHTML削除

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = (): JSX.Element => (
+  <div className="container">
+      <h1>navbar</h1>
+  </div>
+)
+
+export default Navbar
---
+++ app/pages/about.tsx
@@ -0,0 +1,9 @@
+export const About = (): JSX.Element => (
+  <div className="container">
+    <main>
+      <h1>about page</h1>
+    </main>
+  </div>
+)
+
+export default About
